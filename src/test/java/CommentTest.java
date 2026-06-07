0173698286ddda1f6b28810867915452898df54e
Mahsa <Mahsa1386.sor@gmail.com>
2026-06-07 22:32:15 +0330

    Added some test methods.

--- src/test/java/CommentTest.java
+++ src/test/java/CommentTest.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CommentTest {
+
+    @Test
+    void createCommentTest() {
+        LocalDateTime date = LocalDateTime.now();
+        Comment comment = Comment.createComment(
+                1,
+                2,
+                date,
+                "Beautiful photo!"
+        );
+
+        assertEquals(1, comment.getOwnerId());
+        assertEquals(2, comment.getPhotoId());
+        assertEquals(date, comment.getDate());
+        assertEquals("Beautiful photo!", comment.getText());
+    }
+
 }
