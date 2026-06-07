fc297cbe2dece777e8193f6e5b77c061f1cfca13
sara <jafarisara1386@gmail.com>
2026-06-07 21:16:58 +0330

    added enums to user class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,17 @@
 public class User {
+//=============================================ENUM
+    public enum userRank {
+        NEWBIE,
+        PHOTOGRAPHER,
+        COMMENTER,
+        INFLUENCER,
+        ADMIN
+    }
+
+    public enum entreType{
+        phoneNumber,
+        email
+    }
+//=============================================PROPERTIES
 
 }
