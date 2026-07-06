6c61831e17ed11c1939cea545f77439990864a9d
sara <jafarisara1386@gmail.com>
2026-07-06 13:42:38 +0330

    fixed some bugs

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:mora/screens/splash_screen.dart';
-import 'screens/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  static _MyAppState? of(BuildContext context) {
+    return context.findAncestorStateOfType<_MyAppState>();
+  }
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDark = false;
+
+  void changeTheme(bool value) {
+    setState(() {
+      isDark = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Mora',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+
+      theme: ThemeData.light(),
+
+      darkTheme: ThemeData.dark(),
+
+      themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+
       home: const SplashScreen(),
     );
   }
