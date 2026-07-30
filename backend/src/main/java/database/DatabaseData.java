c132f3fe26112ee22ddf6bb09c97e76c5c57d2de
Mahsa <Mahsa1386.sor@gmail.com>
2026-07-30 21:25:52 +0330

    Add DatabaseData

--- backend/src/main/java/database/DatabaseData.java
+++ backend/src/main/java/database/DatabaseData.java
@@ -1,4 +1,39 @@
 package database;
 
+import model.Album;
+import model.Photo;
+import model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DatabaseData {
+
+    private Map<Integer, User> users = new HashMap<>();
+    private Map<Integer, Album> albums = new HashMap<>();
+    private Map<Integer, Photo> photos = new HashMap<>();
+
+    public Map<Integer, User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Map<Integer, User> users) {
+        this.users = users;
+    }
+
+    public Map<Integer, Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(Map<Integer, Album> albums) {
+        this.albums = albums;
+    }
+
+    public Map<Integer, Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(Map<Integer, Photo> photos) {
+        this.photos = photos;
+    }
 }
