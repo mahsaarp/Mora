fa00ea73f03577bbdf109c34b0e03ea8490bbf95
Mahsa <Mahsa1386.sor@gmail.com>
2026-07-31 23:26:01 +0330

    Add gson dependency to pom.xml

--- backend/src/main/java/database/DatabaseManager.java
+++ backend/src/main/java/database/DatabaseManager.java
@@ -1,4 +1,14 @@
 package database;
 
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class DatabaseManager {
+    private static final String FILE_PATH = "database/database.json";
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private DatabaseData databaseData;
+
+    private static volatile DatabaseManager instance;
+
+
 }
