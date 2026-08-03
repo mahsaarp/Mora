4ce8ab676bb0bdb7f48682019ba4ee88637b047f
sara <jafarisara1386@gmail.com>
2026-08-03 23:54:24 +0330

    added features to request class

--- backend/src/main/java/server/Request.java
+++ backend/src/main/java/server/Request.java
@@ -1,4 +1,59 @@
 package server;
 
+import com.google.gson.JsonObject;
+
 public class Request {
+    private String route;
+    private String method;
+    private JsonObject payload;
+
+    public Request() {
+    }
+
+    public Request(String route, String method, JsonObject payload) {
+        this.route = route;
+        this.method = method;
+        this.payload = payload;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public String getAction() {
+        return route;
+    }
+
+    public void setAction(String action) {
+        this.route = action;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public JsonObject getPayload() {
+        return payload;
+    }
+
+    public void setPayload(JsonObject payload) {
+        this.payload = payload;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "route='" + route + '\'' +
+                ", method='" + method + '\'' +
+                ", payload=" + payload +
+                '}';
+    }
 }
--- backend/src/main/java/server/Response.java
+++
@@ -1,6 +0,0 @@
-package server;
-
-public class Response {
-    public Response(int i, String s, Object o) {
-    }
-}
